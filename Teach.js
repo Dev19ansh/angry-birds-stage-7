8f568ec95f2e1f85e476836dfb42b8e420cdc230
Dev19ansh <80335317+Dev19ansh@users.noreply.github.com>
2021-05-25 17:50:16 +0530

    Add files via upload

---
+++ Teach.js
@@ -0,0 +1,8 @@
+
+// No variable can two values
+//To store more than one value on a single variable we use array
+//[] to denote its an array
+//index number - are the positionalm values of the elements inside
+//index starts with zero
+
+var xy=[100,200,2000,40];
